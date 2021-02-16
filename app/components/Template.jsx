97206b46f167b5b9bd054b603fd6c317e85feace
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-02-15 19:22:39 -0600

    Fix format

--- app/components/Template.jsx
+++ app/components/Template.jsx
@@ -1,16 +1,13 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Template(props)
-{
-    return (
-        <>
-          <Head>
-            <title>{props.title}</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-        {
-            props.children
-        }
-        </>
-    )
+export default function Template(props) {
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {props.children}
+    </>
+  );
 }
