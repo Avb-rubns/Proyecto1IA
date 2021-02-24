195823c0bd12f1b89d7de6bb02142e2d6c3459ea
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-02-24 15:34:59 -0600

    se paso a switch

--- app/pages/api/users.js
+++ app/pages/api/users.js
@@ -1,26 +1,34 @@
 import { MongoDBService } from "../../services/mongodb";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { register } = req.query;
-    //console.log(register);
-    if (register) {
-      const { data } = req.body;
-      console.log({ recibiendoda: data });
-      const service = new MongoDBService();
-      const result = await service.register(data);
-      service.close();
-      res.status(200).send({ result: result });
-    } else {
-      const { form } = req.body;
-      const service = new MongoDBService();
-      const result = await service.login(form);
-      service.close();
-      res.status(200).send({ result: result });
-    }
-  } else {
-    res.status(404).send({
-      result: "error!! ",
-    });
+  switch (req.method) {
+    case "GET":
+      break;
+    case "POST":
+      const { register } = req.query;
+      if (register) {
+        const { data } = req.body;
+        console.log({ recibiendoda: data });
+        const service = new MongoDBService();
+        const result = await service.register(data);
+        service.close();
+        res.status(200).send({ result: result });
+      } else {
+        const { form } = req.body;
+        const service = new MongoDBService();
+        const result = await service.login(form);
+        service.close();
+        res.status(200).send({ result: result });
+      }
+      break;
+    case "PUT":
+      break;
+    case "PATCH":
+      break;
+    case "DELETE":
+      break;
+
+    default:
+      break;
   }
 }
