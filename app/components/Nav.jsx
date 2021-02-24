7ec020949c4fc3fb06702ad507484e03a20dee50
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-02-24 15:34:12 -0600

    se creo el nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,13 +1,35 @@
-import Head from "next/head";
+import { Layout, Button, Typography, Avatar } from "antd";
+import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
+import { blue } from "@ant-design/colors";
+import styles from "../styles/planear.module.css";
+
+const { Title } = Typography;
+
+const { Header } = Layout;
 
 export default function Nav(props) {
   return (
     <>
-      <Head>
-        <title>{props.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {props.children}
+      <Header
+        style={{
+          background: blue[5],
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Title level={3}>Dline</Title>
+        <div className={styles["info-avatar"]}>
+          <Avatar size="small" icon={<UserOutlined />} />
+          <p style={{ marginLeft: "1rem", color: "#ffffff" }}>{props.name}</p>
+          <Button
+            style={{ color: "white" }}
+            type="text"
+            icon={<LogoutOutlined />}
+            onClick={props.exit}
+          ></Button>
+        </div>
+      </Header>
     </>
   );
 }
