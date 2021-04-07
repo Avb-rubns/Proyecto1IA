b72b1daf494fb3a09f4333fb1dc6b04b173aeb92
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-04-07 18:21:15 -0500

    insert delivery in user andd rep

--- app-refact/pages/api/directioninfo.js
+++ app-refact/pages/api/directioninfo.js
@@ -2,21 +2,34 @@ import { formatText } from "../../scripts/format";
 import { MapsService } from "../../services/maps";
 import { MongoDBService } from "../../services/mongo";
 export default async function handler(req, res) {
+  const serviceMaps = new MapsService();
+  const textF = new formatText();
+  const serviceMongo = new MongoDBService();
+
   switch (req.method) {
     case "POST":
       const { entrega } = req.body;
-      const textF = new formatText();
       const des = textF.createDirection(entrega);
       const id = textF.createID(entrega);
 
-      const serviceMaps = new MapsService();
       const info = await serviceMaps.getInfo(des);
 
       const delivery = textF.createDelivery(info, id, entrega);
-      const serviceMongo = new MongoDBService();
-      const result = await serviceMongo.insertDelivery(delivery);
+      if (entrega.idUser != "") {
+        const register = true;
+        await serviceMongo.insertDelivery(delivery, "", register);
+      } else {
+        const register = false;
+        await serviceMongo.insertDelivery(delivery, "", register);
+      }
       serviceMongo.close();
       res.status(200).send(delivery);
+      break;
+
+    case "DELETE":
+      const r = await serviceMongo.deleteDelivery(req.query);
+      serviceMongo.close();
+      res.status(200).send({ r });
       break;
 
     default:
