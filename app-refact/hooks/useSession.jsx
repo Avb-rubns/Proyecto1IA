d551b9ab1738e578bb241fe6b0c9cc43b1fb10f5
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-04-27 20:16:19 -0500

    se quito usecallback

--- app-refact/hooks/useSession.jsx
+++ app-refact/hooks/useSession.jsx
@@ -1,9 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import Context from "../context/sessionContext";
 import { parseCookies, setCookie, destroyCookie } from "nookies";
 
 export default function useSession() {
-  const [loading, setLoading] = useState(true);
   const { token, setToken } = useContext(Context);
 
   useEffect(() => {
@@ -13,30 +12,27 @@ export default function useSession() {
     }
   }, []);
 
-  const login = useCallback(
-    async ({ sesion }) => {
-      try {
-        const result = await fetch("http://localhost:3000/api/user", {
-          method: "POST",
-          headers: {
-            "content-Type": "application/json",
-          },
-          body: JSON.stringify({ sesion }),
-        }).then((res) => res.json());
+  const login = async ({ sesion }) => {
+    try {
+      const result = await fetch("http://localhost:3000/api/user", {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify({ sesion }),
+      }).then((res) => res.json());
 
-        if (token != "") {
-          setCookie(null, "token", result.token, {
-            maxAge: 30 * 24 * 60 * 60,
-            path: "/",
-          });
-          setToken(result);
-        }
-      } catch (error) {
-        console.log("Error:" + error);
+      if (token != "") {
+        setCookie(null, "token", result.token, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: "/",
+        });
+        setToken(result);
       }
-    },
-    [setToken]
-  );
+    } catch (error) {
+      console.log("Error:" + error);
+    }
+  };
 
   const logOut = () => {
     try {
@@ -51,7 +47,5 @@ export default function useSession() {
     isLogged: Boolean(token),
     login,
     logOut,
-    loading,
-    setLoading,
   };
 }
